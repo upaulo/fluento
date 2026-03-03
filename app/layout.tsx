c93c794cd2af93dd0e7b392bfcdf7a26fb7ac9f9
upaulo <paulojr.hs@gmail.com>
2026-03-02 23:45:33 -0300

    setup-project

---
+++ app/layout.tsx
@@ -0,0 +1,22 @@
+import type { Metadata } from "next";
+import { Nunito } from "next/font/google";
+import "./globals.css";
+
+const font = Nunito({ subsets: ["latin"] });
+
+export const metadata: Metadata = {
+	title: "Fluento App",
+	description: "Learn new languages",
+};
+
+export default function RootLayout({
+	children,
+}: Readonly<{
+	children: React.ReactNode;
+}>) {
+	return (
+		<html lang="en">
+			<body className={`${font.className} antialiased`}>{children}</body>
+		</html>
+	);
+}
---
+++ app/page.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+	return (
+		<div>
+			<p className="text-green-500 font-bold">Fluento</p>
+		</div>
+	);
+}
