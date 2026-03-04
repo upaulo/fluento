40c9cf8a070cd6736aaee31509c7c42519cec8ef
upaulo <paulojr.hs@gmail.com>
2026-03-04 11:59:59 -0300

    feat(marketing): create page

---
+++ app/(marketing)/header.tsx
@@ -0,0 +1,9 @@
+export const Header = () => {
+	return (
+		<div>
+			<header className="h-20 w-full border-2 border-slate-200 px-4">
+				header
+			</header>
+		</div>
+	);
+};
--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,7 @@
 export default function Home() {
 	return (
 		<div>
-			<p className="text-green-500 font-bold">Fluento</p>
+			<p className="text-green-500 font-bold">Marketing</p>
 		</div>
 	);
 }
