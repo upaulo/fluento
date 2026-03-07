ed398b01360f0c2d23ae0ba15adc391b1a5472e9
upaulo <paulojr.hs@gmail.com>
2026-03-06 23:42:55 -0300

    feat(marketing): update structure and style

--- app/(marketing)/page.tsx
+++ app/(marketing)/page.tsx
@@ -1,7 +1,50 @@
+import {
+	ClerkLoaded,
+	ClerkLoading,
+	Show,
+	SignInButton,
+	SignUpButton,
+} from "@clerk/nextjs";
+import { Loader } from "lucide-react";
+import Image from "next/image";
+import Link from "next/link";
+import { Button } from "@/components/ui/button";
+
 export default function Home() {
 	return (
-		<div>
-			<p className="text-green-500 font-bold">Marketing</p>
+		<div className="max-w-[998px] mx-auto flex-1 w-full flex flex-col lg:flex-row items-center justify-center p4 gap-2">
+			<div className="relative w-[240px] h-[240px] lg:w-[424px] lg:h-[424px] mb-8 lg:mb-0">
+				<Image src="/hero.svg" alt="Hero" fill />
+			</div>
+			<div className="flex flex-col items-center gap-y-8">
+				<h1 className="text-xl lg:text-3xl font-bold text-neutral-600 max-w-[480px] text-center">
+					Learn, pratice and master new languages with Fluento!
+				</h1>
+				<div className="flex flex-col items-center gap-y-3 max-w-[330px] w-full">
+					<ClerkLoading>
+						<Loader className="h-5 w-5 text-muted-foreground animate-spin" />
+					</ClerkLoading>
+					<ClerkLoaded>
+						<Show when="signed-out">
+							<SignUpButton mode="modal">
+								<Button size="lg" variant="secondary" className="w-full">
+									Get Started
+								</Button>
+							</SignUpButton>
+							<SignInButton mode="modal">
+								<Button size="lg" variant="primaryOutline" className="w-full">
+									I already have a account
+								</Button>
+							</SignInButton>
+						</Show>
+						<Show when="signed-in">
+							<Button size="lg" variant="secondary" className="w-full" asChild>
+								<Link href="/learn">continue learning</Link>
+							</Button>
+						</Show>
+					</ClerkLoaded>
+				</div>
+			</div>
 		</div>
 	);
 }
