8a6a2260cf988e926dfe0d7a3da2019770f64342
upaulo <paulojr.hs@gmail.com>
2026-03-08 01:37:07 -0300

    feat(mobile): create reusable mobile header and sidebar component

---
+++ components/mobile-header.tsx
@@ -0,0 +1,11 @@
+import MobileSidebar from "@/components/mobile-sidebar";
+
+function MobileHeader() {
+	return (
+		<div className="lg:hidden px-6 h-[50px] flex items-center bg-green-500 border-b fixed top-0 w-full z-50">
+			<MobileSidebar />
+		</div>
+	);
+}
+
+export default MobileHeader;
---
+++ components/mobile-sidebar.tsx
@@ -0,0 +1,18 @@
+import { Menu } from "lucide-react";
+import Sidebar from "@/components/sidebar";
+import { Sheet, SheetContent, SheetTrigger } from "@/components/ui/sheet";
+
+function MobileSidebar() {
+	return (
+		<Sheet>
+			<SheetTrigger>
+				<Menu className="text-white" />
+			</SheetTrigger>
+			<SheetContent className="p-0 z-100" side="left">
+				<Sidebar />
+			</SheetContent>
+		</Sheet>
+	);
+}
+
+export default MobileSidebar;
