8972da27876cfae2bbb300fb55fa176ad2356326
upaulo <paulojr.hs@gmail.com>
2026-03-09 18:55:01 -0300

    feat(learn): update structure and style

--- app/(main)/learn/page.tsx
+++ app/(main)/learn/page.tsx
@@ -1,5 +1,21 @@
+import FeedWrapper from "@/components/feed-wrapper";
+import StickyWrapper from "@/components/sticky-wrapper";
+
 function LearnPage() {
-	return <div>learn page</div>;
+	return (
+		<div className="flex flex-row-reverse gap-[48px] px-6">
+			<StickyWrapper>
+				<div className="h-[100px] w-full bg-red-500">sticky sidebar</div>
+				<div className="h-[100px] w-full bg-red-500">sticky sidebar</div>
+				<div className="h-[100px] w-full bg-red-500">sticky sidebar</div>
+			</StickyWrapper>
+			<FeedWrapper>
+				<div className="h-[200px] w-full bg-blue-500 mb-1">feed</div>
+				<div className="h-[200px] w-full bg-blue-500 mb-1">feed</div>
+				<div className="h-[200px] w-full bg-blue-500 mb-1">feed</div>
+			</FeedWrapper>
+		</div>
+	);
 }
 
 export default LearnPage;
