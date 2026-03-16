9ac667769097504977a9a9bba328e7aef813ed9b
upaulo <paulojr.hs@gmail.com>
2026-03-16 10:55:27 -0300

    feat(lesson): add lesson exit modal

--- app/lesson/header.tsx
+++ app/lesson/header.tsx
@@ -1,6 +1,7 @@
 import { InfinityIcon, X } from "lucide-react";
 import Image from "next/image";
 import { Progress } from "@/components/ui/progress";
+import { useExitModal } from "@/store/use-exit-modal";
 
 type Props = {
 	hearts: number;
@@ -9,11 +10,13 @@ type Props = {
 };
 
 function Header({ hearts, percentage, hasActiveSubscription }: Props) {
+	const { open } = useExitModal();
+
 	return (
 		<header className="lg:pt-[50px] pt-[20px] px-10 flex gap-7 items-center justify-between max-w-[1140px] mx-auto w-full">
 			<X
 				className="text-slate-500 hover:opacity-75 transition cursor-pointer"
-				onClick={() => {}}
+				onClick={open}
 			/>
 			<Progress value={percentage} />
 			<div className="text-rose-500 flex items-center font-bold">
